c8f6a78a9ca179ae265598e014c3b179d6057a3e
jlnLNT <julienlentmail@gmail.com>
2022-11-21 19:05:38 +0000

    test2

--- web-service/test_app.py
+++ web-service/test_app.py
@@ -5,9 +5,8 @@ location = {
     "lon": 6.1,
 }
 
-url = 'http://localhost:9696/predict'
+url = "http://localhost:9696/predict"
 
 url = "https://61bmqgucoe.execute-api.eu-west-3.amazonaws.com/postLocation"
 response = requests.post(url, json=location)
 print(response.json())
-
